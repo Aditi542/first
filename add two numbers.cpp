d444e4d00debad9d467b2a76609c778663d03213
Aditi542 <89867911+Aditi542@users.noreply.github.com>
2021-10-17 00:23:49 +0530

    Add files via upload

---
+++ add two numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a; // Variable declaration
+    int b;
+    int sum;
+    cout<<"Enter one number : ";         // message asking for input
+    cin>>a;                              // taking input
+    cout<<endl<<"Enter two number : ";   // message asking for second input
+    cin>>b;                              // taking input
+    sum=a+b;                             // adding two variable and storing it into third variable sum
+    cout<<endl<<"Sum is "<<sum;          //printing the output to the screen
+    cout<<endl;
+
+    return 0;
+}
